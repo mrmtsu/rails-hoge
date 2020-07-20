1d5831b25a7da2c986bfe96cf4bb0a0adafcbc46
mrmtsu <81202muramatsu@gmail.com>
2020-07-20 17:25:50 +0900

    Rspec導入

--- config/application.rb
+++ config/application.rb
@@ -11,9 +11,18 @@ module AppName
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
 
+    config.generators do |g|
+      g.test_framework :rspec,
+        controller_specs: false,
+        view_specs: false,
+        helper_specs: false,
+        routing_specs: false
+      end
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+    end
   end
 end
